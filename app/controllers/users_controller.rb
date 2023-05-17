8d1a7464f9fbee7d8b9a1b39de28d6ad78db1a54
teishingi <shingi.cx@gmail.com>
2023-05-17 20:22:36 +0900

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,21 +8,26 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def show
-    @user = User.find(params[:id])
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
-  def index
-    @users = User.paginate(page: params[:page], per_page: 15)
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to user_url(@user)
+      # reset_session
+      # log_in @user
+      # flash[:success] = "Welcome to the Sample App!"
+      # redirect_to user_url(@user)
+      
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
     end
